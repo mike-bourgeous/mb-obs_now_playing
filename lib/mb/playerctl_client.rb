86b5dd4754573d3f33d5b08a37eb24c7ddf9e7d3
Mike Bourgeous <mike@mikebourgeous.com>
2025-09-03 16:01:39 -0600

    Separate playerctl test from the client class.

--- lib/mb/playerctl_client.rb
+++ lib/mb/playerctl_client.rb
@@ -114,12 +114,3 @@ class PlayerctlClient
     }.to_h
   end
 end
-
-PlayerctlClient.new do |d|
-  app, v = d.detect { |app, h| h[:status] == 'Playing' }
-  if v
-    puts "\e[33m#{app} playing \e[1m#{[v[:album], v[:artist], v[:title]].compact.join(' - ')}\e[0m"
-  else
-    puts "\e[34mpaused\e[0m"
-  end
-end
